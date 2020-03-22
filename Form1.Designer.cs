465945a0f91aed903e73ff8f9fd4b2b5b08d53bb
choppyratz <30554606+choppyratz@users.noreply.github.com>
2020-03-22 23:52:46 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace HttpClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pOSTParametrBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pOSTParametrBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(776, 390);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "HEAD"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 13);
+            this.textBox2.MinimumSize = new System.Drawing.Size(4, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(477, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Http Client";
+            ((System.ComponentModel.ISupportInitialize)(this.pOSTParametrBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource pOSTParametrBindingSource;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace HttpClient
+{
+    public partial class Form1 : Form
+    {
+        public TcpClient TcpHandle;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            string request = "";
+            string uri = getUri(textBox2.Text);
+            string host = getHostName(textBox2.Text);
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    request = GETMethod(host, uri);
+                    break;
+                case 1:
+                    request = POSTMethod(host, getPOSTParametrs(ref uri), uri);
+                    break;
+                case 2:
+                    request = HEADMethod(host, uri);
+                    break;
+            }
+            textBox1.Text += request + "\r\n";
+            TcpHandle = new TcpClient(getHostName(textBox2.Text), 80);
+            NetworkStream stream = TcpHandle.GetStream();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(request);
+            stream.Write(data, 0, data.Length);
+
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            textBox1.Text += reader.ReadToEnd();
+            stream.Close();
+            TcpHandle.Close();
+        }
+
+
+        public string getUri(string url)
+        {
+            Regex RegExUri = new Regex("(?<=[a-zA-Z])/.*");
+            MatchCollection matches = RegExUri.Matches(url);
+            string uri = "/";
+            foreach (Match match in matches)
+                uri = match.Value;
+            return uri;
+        }
+
+        public string getHostName(string url)
+        {
+            Regex RegExUri = new Regex("(?<=(//)|^).+.[A-Za-z]{2,3}(?=/)");
+            MatchCollection matches = RegExUri.Matches(url);
+            string host = "";
+            foreach (Match match in matches)
+                host = match.Value;
+            host = host.Replace("https://", "");
+            host = host.Replace("http://", "");
+            int fSlash = host.IndexOf("/");
+            if (fSlash != -1)
+                host = host.Substring(0, fSlash);
+            return host;
+        }
+
+        public string getPOSTParametrs(ref string uri)
+        {
+            //(?<=\?|&)[A-Za-z0-9_-]+
+            //(?<=\=)[A-Za-z0-9-_]+
+            //(?<=\?).+
+            Regex ParamsRegex = new Regex("(?<=[?]).+");
+            MatchCollection mathces = ParamsRegex.Matches(uri);
+            string POSTParams = "";
+            foreach (Match match in mathces)
+            {
+                POSTParams = match.Value;
+            }
+            uri = uri.Remove(uri.Length - POSTParams.Length - 1, POSTParams.Length + 1);
+
+            return POSTParams;
+        }
+
+
+        public string GETMethod(string url, string uri)
+        {
+            string mainOption = "{0} {1} HTTP/1.0\r\n";
+            string hostOption = "Host: {0}\r\n";
+            string acceptOption = "Accept: {0}\r\n";
+            string connectionOption = "Connection: {0}\r\n";
+            string userAgentOption = "User-Agent: {0}\r\n";
+            string lastLineOption = "\r\n";
+
+            return String.Format(mainOption, "GET", uri) + 
+                   String.Format(hostOption, url) + 
+                   String.Format(acceptOption, "text/html") + 
+                   String.Format(connectionOption, "close") + 
+                   String.Format(userAgentOption, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36") +
+                   lastLineOption;
+        }
+
+        public string POSTMethod(string url, string parametrs, string uri)
+        {
+            string mainOption = "{0} {1} HTTP/1.0\r\n";
+            string hostOption = "Host: {0}\r\n";
+            string acceptOption = "Accept: {0}\r\n";
+            string connectionOption = "Connection: {0}\r\n";
+            string userAgentOption = "User-Agent: {0}\r\n";
+            string contentTypeOption = "Content-type: {0}\r\n";
+            string contentLengthOption = "Content-length: {0}\r\n";
+            string lastLineOption = "\r\n";
+
+            return String.Format(mainOption, "POST", uri) + 
+                   String.Format(hostOption, url) + String.Format(acceptOption, "text/html") +
+                   String.Format(contentTypeOption, "application/x-www-form-urlencoded") + 
+                   String.Format(connectionOption, "keep-alive") +
+                   String.Format(contentLengthOption, parametrs.Length) +
+                   String.Format(userAgentOption, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36") +
+                   lastLineOption + 
+                   parametrs +
+                   lastLineOption;
+
+        }
+
+        public string HEADMethod(string url, string uri)
+        {
+            string mainOption = "{0} {1} HTTP/1.0\r\n";
+            string hostOption = "Host: {0}\r\n";
+            string acceptOption = "Accept: {0}\r\n";
+            string connectionOption = "Connection: {0}\r\n";
+            string userAgentOption = "User-Agent: {0}\r\n";
+            string lastLineOption = "\r\n";
+            return String.Format(mainOption, "HEAD", uri) + 
+                   String.Format(hostOption, url) + 
+                   String.Format(acceptOption, "text/html") + 
+                   String.Format(connectionOption, "keep-alive") + 
+                   String.Format(userAgentOption, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36") +
+                   lastLineOption;
+
+        }
+
+    }
+}
